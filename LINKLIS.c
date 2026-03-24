be9b82e0a92e3607165127233711397754ac3638
Arun <arunbaigra4@gmail.com>
2026-03-24 12:26:59 +0530

    Added functions for reversing the linked list 1. REversing the elements ,2. Reversing the links ,3. Recursive reversing the links

--- LINKLIS.c
+++ LINKLIS.c
@@ -219,12 +219,60 @@ void RemoveDuplicate(struct Node *p)
     }
 }
 
+void Reverse1(struct Node *p)
+{
+    int *A,i;
+    struct Node *q;
+
+    A=(int *)malloc(sizeof(int)*count(p));
+    while(q!=NULL)
+
+    {
+        A[i] =q->data;
+        q=q->next;
+        i++;    
+    }
+    q=p;
+    i--;
+    while(q!=NULL)
+    {
+        q->data=A[i];
+        q=q->next;
+        i--;
+    }
+
+}
+void Reverse2(struct Node *p)
+{
+    struct Node *q=NULL,*r=NULL;
+    while(p!=NULL)
+    {
+        r=q;
+        q=p;
+        p=p->next;
+        q->next=r;
+    }
+    first =q;
+}
+
+void Reverse3(struct Node *q, struct Node *p)
+{
+    if(p)
+    {
+        Reverse3(p,p->next);
+        p->next=q;
+    }
+    else
+        first = q;
+}
+
 int main()
 {
     struct Node *temp;
     int A[]={3,5,10,10,15,15,88,96};
     create(A,8);
-    RemoveDuplicate(first);
+    //RemoveDuplicate(first);
+    Reverse3(NULL,first);
     Display(first);
     printf("\n\n");
 
