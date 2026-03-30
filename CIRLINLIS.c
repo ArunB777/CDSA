769f71087dacf55ea7e45227c5ee3d2a67f873f2
Arun <arunbaigra4@gmail.com>
2026-03-30 14:10:14 +0530

    added insert and delete functions to the circular linked list

--- CIRLINLIS.c
+++ CIRLINLIS.c
@@ -14,7 +14,7 @@ void create(int A[],int n)
     struct Node *t, *last;
     Head = (struct Node*)malloc(sizeof(struct Node));
     Head->data = A[0];
-    Head->next = NULL;
+    Head->next = Head;
     last = Head;
     for(i=1;i<n;i++)
     {
@@ -27,12 +27,13 @@ void create(int A[],int n)
 }
 
 void Display(struct Node *h)
-{
+{ 
+    struct Node *p = h;
     do
     {
-        printf("%d ",h->data);
-        h=h->next;
-    } while (h!=NULL);
+        printf("%d ",p->data);
+        p=p->next;
+    } while (p!=h);
     
 }
 void RDisplay(struct Node *h)
@@ -48,13 +49,95 @@ void RDisplay(struct Node *h)
     flag =0;
 }
 
+int Length(struct Node *p)
+{
+    int len=0;
+    do
+    {
+        len++;
+        p=p->next;
+    }while(p!=Head);
+    return len;
+}
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+    
+    if(index<0 || index > Length(p))
+        return;
+    if(index<0 )
+    {
+        t= (struct Node *)malloc(sizeof(struct Node));
+        t->data=x;
+        if(Head==NULL)
+        {
+            Head = t;
+            Head->next=Head;
+        }
+        else
+        {
+            while(p->next!=Head) p=p->next;
+                p->next=t;
+                t->next=Head;
+                Head=t;
 
+        }
+
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)p=p->next;
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=x;
+        t->next=p->next;
+        p->next=t;
+    }
+}
+
+int Delete(struct Node *p,int index)
+{
+    struct Node *q;
+    int i,x;
+    if(index <0 || index > Length(Head))
+        return -1;
+    if(index==1)
+    {
+        while(p->next!=Head)p=p->next;
+        x=Head->data;
+        if(Head==p)
+        {
+            free(Head);
+            Head=NULL;
+        }
+        else
+        {
+            p->next=Head->next;
+            free(Head);
+            Head=p->next;
+        }
+    }
+    else
+    {
+        for(i=0;i<index-2;i++)p=p->next;
+        q=p->next;
+        p->next=q->next;
+        x=q->data;  
+        free(q);  
+    
+    }
+    return x;
+
+
+}
 int main()
 {
     int A[]={3,5,7,10,15,20,25,30};
     int B[]={4,8,12,16,20};
     create(A,8);
+    Insert(Head,6,10);
     //Display(Head);
+    Delete(Head,2);
     RDisplay(Head);
     return 0;
 }
